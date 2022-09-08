a805c72c595a7a540e492b967f6f1f4df062741e
Anderson Bispo <anderson.bispo@4linux.com.br>
2022-09-08 10:02:13 -0300

    provider

--- 01-aulas/aula01/provider.tf
+++ 01-aulas/aula01/provider.tf
@@ -10,4 +10,5 @@ terraform {
 provider "google" {
   project     = "terraform-4linux-8253"
   region      = "southamerica-east1"
+  credentials = "/Users/anderson/Documents/gcp-svc/terraform-4linux-8253-f6be2bb58711.json"
 }
