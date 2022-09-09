f10589beb7c61e9349296de5cd702fdfd80bb881
Anderson Bispo <anderson.bispo@4linux.com.br>
2022-09-09 09:32:39 -0300

    aula04

---
+++ 01-aulas/aula04/subnet.tf
@@ -0,0 +1,14 @@
+resource "google_compute_subnetwork" "subnet1" {
+  name          = "vpc-network"
+  ip_cidr_range = "10.10.0.0/22"
+  region        = "us-central1"
+  network       = "vpc-network"
+}
+
+resource "google_compute_subnetwork" "subnet2" {
+  name          = "vpc-network"
+  ip_cidr_range = "10.10.4.0/22"
+  region        = "southamerica-east1"
+  network = "vpc-network"
+}
+
