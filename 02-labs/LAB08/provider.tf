da5ac1b14a96764f982cd3a9bfa6b662c015cbe6
Anderson Bispo <anderson.bispo@4linux.com.br>
2022-09-16 11:09:48 -0300

    lab08

---
+++ 02-labs/LAB08/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.35.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "terraform-4linux-8253"
+  region      = "southamerica-east1"
+  credentials = "/Users/anderson/Documents/gcp-svc/terraform-4linux-8253-f6be2bb58711.json"
+}
